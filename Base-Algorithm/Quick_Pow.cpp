900e9fe448adda12d299dd9c5dcf581a36a887fc
tllwtg <tllwtg@gmail.com>
2024-09-01 16:15:17 +0800

    Update.

--- Base-Algorithm/Quick_Pow.cpp
+++ Base-Algorithm/Quick_Pow.cpp
@@ -39,3 +39,49 @@ ll qpow(ll a, ll t, ll mod)
 // if mod is a prime number
 // t %= (mod - 1);
 // a^(t^tt) == a^(t^tt % (mod - 1))
+
+// 矩阵快速幂
+namespace MatPow
+{
+    const ll N = 105, MOD = 1e9 + 7;
+    int n = 10;
+
+    struct Mat
+    {
+        ll v[N][N];
+    };
+
+    Mat operator *(const Mat &x, const Mat &y)
+    {
+        Mat ans;
+        ll tmp = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+            {
+                tmp = 0;
+                for (int k = 0; k < n; ++k)
+                    tmp += x.v[i][k] * y.v[k][j] % MOD;
+                ans.v[i][j] = tmp % MOD;
+            }
+        return ans;
+    }
+
+    Mat qpow(Mat a, ll k)
+    {
+        Mat res;
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                res.v[i][j] = 0;
+        for (int i = 0; i < n; ++i)
+            res.v[i][i] = 1;
+
+        while(k)
+        {
+            if (k & 1)
+                res = res * a;
+            a = a * a;
+            k >>= 1;
+        }
+        return res;
+    }
+}
